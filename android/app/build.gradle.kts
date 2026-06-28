9dcf09ec0d4b9ea8c6e0f786f210ad934f06e2aa
chriskito30-a11y <chriskito30-a11y@users.noreply.github.com>
2026-06-28 17:04:23 +0200

    Target modern Android SDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "fr.magiclockscreen.android"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 3
         versionName = "0.3.0"
     }
